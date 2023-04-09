00ab137dc080e6731ab5b78bd62fb954c0b09e98
DarkWinD <vigabriel.darwin@hotmail.com>
2023-04-08 22:28:01 -0400

    se modifico el html y se creo el css

--- resources/views/estudiante.blade.php
+++ resources/views/estudiante.blade.php
@@ -5,14 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Importar-Exportar</title>
+    <link rel="preload" href="/resources/css/app.css" as="style">
+    <link href="/resources/css/app.css" rel="stylesheet">
 </head>
 <body>
    <h1>IMPORTAR DATOS</h1>
     <form action="{{url('estudiante/importar')}}" method="POST" enctype="multipart/form-data">
-     @csrf
-        @if (Session::has('menssage'))
-         <p>{{Session::get('menssage')}}</p> 
-      @endif
       <input type="file" name="documento">
       <button>importar</button>
       <H1>La negra hace 2 x 1 harry!!!!</H1>
